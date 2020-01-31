28cc199a365eed32335f6fe1dd1a98e3b6c9da64
Filip Napiorkowski <36730235+majortilt@users.noreply.github.com>
2020-01-31 20:34:56 +0000

    Added comments

--- unluckyrevised.py
+++ unluckyrevised.py
@@ -1,8 +1,8 @@
-for num in range(0,21):
+for num in range(0,21): #looping through numbers 1 - 20
     if num == 4 or num == 13:
-        state = "unlucky"
+        state = "unlucky" #assign the state if the number matches condition
     elif num % 2 == 0:
         state = "even"
     else:
         state = "odd"
-    print(f"{num} is {state}")
+    print(f"{num} is {state}") #formatted string which prints the number and the associated state
